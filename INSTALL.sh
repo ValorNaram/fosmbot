4ba157b3ba852f41fbb893298eff7de746510c2a
ValorNaram <valinora@gmx.net>
2020-08-10 22:05:47 +0200

    fixed install commands

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 apt-get install python3 python3-pip
 apt-get install postgresql
 
-pip install -U tgcrypto
-pip install -U https://github.com/pyrogram/pyrogram/archive/asyncio.zip
-pip install pyyaml
-pip install psycopg2
+pip3 install -U tgcrypto
+pip3 install -U https://github.com/pyrogram/pyrogram/archive/asyncio.zip
+pip3 install pyyaml
+pip3 install psycopg2
