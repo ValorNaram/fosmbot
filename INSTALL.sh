3039b31bd4af772344e51d97d8b0e434ebd7b4da
ValorNaram <valinora@gmx.net>
2020-08-10 22:15:59 +0200

    fix install.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,5 @@
 apt-get install python3 python3-pip
-apt-get install postgresql
+apt-get install postgresql, libpq-dev
 
 pip3 install -U tgcrypto
 pip3 install -U https://github.com/pyrogram/pyrogram/archive/asyncio.zip
