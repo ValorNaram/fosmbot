8ba900e90d0ecc6eb8bac30fe4fd0d063dc0e157
ValorNaram <valinora@gmx.net>
2020-08-10 18:21:25 +0200

    add install

---
+++ INSTALL.sh
@@ -0,0 +1,7 @@
+apt-get install python3 python3-pip
+apt-get install postgresql
+
+pip install -U tgcrypto
+pip install -U https://github.com/pyrogram/pyrogram/archive/asyncio.zip
+pip install pyyaml
+pip install psycopg2
